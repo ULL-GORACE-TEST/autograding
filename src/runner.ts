2fed7df9205abca368ccdabb937ad7afbf4f6092
Claudio Yanes <me@claudio4.com>
2024-02-26 23:45:40 +0000

    Add activity to utput

--- src/runner.ts
+++ src/runner.ts
@@ -268,11 +268,12 @@ export const runAll = async (tests: Array<Test>, cwd: string): Promise<void> => 
 
   // Set the number of points
   if (hasPoints) {
-    const resultsStr = JSON.stringify(results)
+    const output = { activty: core.getInput('activity'), results }
+    const outputStr = JSON.stringify(output)
     const text = `Points ${points}/${availablePoints}`
     log(color.bold.bgCyan.black(text))
     core.setOutput('Points', `${points}/${availablePoints}`)
-    core.setOutput('TestResults', resultsStr)
-    await setCheckRunOutput(resultsStr)
+    core.setOutput('TestResults', outputStr)
+    await setCheckRunOutput(outputStr)
   }
 }
